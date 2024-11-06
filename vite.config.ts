b14e7c91ce53a7bc341039b8f97db2399fba9806
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2024-11-06 11:45:40 -0600

    chore: fixing gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/palestinian-archive/",
+  base: "./palestinian-archive/",
   plugins: [react()],
   resolve: {
     alias: {
